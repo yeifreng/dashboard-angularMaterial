e398a3718aca34a993230a6981029dd95f47674b
Yeifreng <yeifrenjose@gmail.com>
2023-10-28 09:10:55 -0500

    modificacion del diseño y creacion de reactiveForm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
+import { ListaEmpleadoComponent } from './lista-empleado/lista-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: 'dashboard', pathMatch: 'full' },
+  {path:'dashboard',component:DashboardComponent},
+  {path:'agregar-empleado',component:AgregarEmpleadoComponent},
+  {path:'lista-empleado',component:ListaEmpleadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
